114d84a4a877e3f83491d42d0ed897c1885cff01
Diego Ferreira <diegofs01@hotmail.com>
2020-06-28 14:15:19 -0300

    Update Cliente model

--- src/main/java/com/diego/venda/model/Cliente.java
+++ src/main/java/com/diego/venda/model/Cliente.java
@@ -1,5 +1,7 @@
 package com.diego.venda.model;
 
+import java.sql.Date;
+
 import javax.persistence.Entity;
 import javax.persistence.Id;
 
@@ -9,30 +11,102 @@ public class Cliente {
 	@Id
 	private String cpf;
 	private String nome;
+	private Date dataNascimento;
+	private String sexo;
+	private String cep;
+	private String logradouro;
+	private int numero;
+	private String complemento;
+	private String bairro;
+	private String cidade;
+	private String uf;	
 	private double saldo;
 	private boolean ativo;
-	
+
 	public String getCpf() {
 		return cpf;
 	}
 	public void setCpf(String cpf) {
 		this.cpf = cpf;
 	}
-	
+
 	public String getNome() {
 		return nome;
 	}
 	public void setNome(String nome) {
 		this.nome = nome;
 	}
-	
+
+	public Date getDataNascimento() {
+		return dataNascimento;
+	}
+	public void setDataNascimento(Date dataNascimento) {
+		this.dataNascimento = dataNascimento;
+	}
+
+	public String getSexo() {
+		return sexo;
+	}
+	public void setSexo(String sexo) {
+		this.sexo = sexo;
+	}
+
+	public String getCep() {
+		return cep;
+	}
+	public void setCep(String cep) {
+		this.cep = cep;
+	}
+
+	public String getLogradouro() {
+		return logradouro;
+	}
+	public void setLogradouro(String logradouro) {
+		this.logradouro = logradouro;
+	}
+
+	public int getNumero() {
+		return numero;
+	}
+	public void setNumero(int numero) {
+		this.numero = numero;
+	}
+
+	public String getComplemento() {
+		return complemento;
+	}
+	public void setComplemento(String complemento) {
+		this.complemento = complemento;
+	}
+
+	public String getBairro() {
+		return bairro;
+	}
+	public void setBairro(String bairro) {
+		this.bairro = bairro;
+	}
+
+	public String getCidade() {
+		return cidade;
+	}
+	public void setCidade(String cidade) {
+		this.cidade = cidade;
+	}
+
+	public String getUf() {
+		return uf;
+	}
+	public void setUf(String uf) {
+		this.uf = uf;
+	}
+
 	public double getSaldo() {
 		return saldo;
 	}
 	public void setSaldo(double saldo) {
 		this.saldo = saldo;
 	}
-	
+
 	public boolean isAtivo() {
 		return ativo;
 	}
